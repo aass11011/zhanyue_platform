fc8510645efbf688ee4a9685f73849341eb4ff59
aass11011 <381766867@qq.com>
2026-06-11 23:42:54 +0800

    后台修改

---
+++ platform-portal/src/main/java/com/zym/fastplatform/portal/system/controller/SysDictDataController.java
@@ -0,0 +1,23 @@
+package com.zym.fastplatform.portal.system.controller;
+
+import com.zym.fastplatform.framework.entity.Result;
+import com.zym.fastplatform.system.entity.vo.SysDictDataVO;
+import com.zym.fastplatform.system.service.SysDictDataService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/sysDictData")
+@Slf4j
+public class SysDictDataController {
+    @Autowired
+    private SysDictDataService sysDictDataService;
+
+    @GetMapping("listByType")
+    public Result<List<SysDictDataVO>> listByType(@RequestParam("typeId") String typeId) {
+        return Result.ok(sysDictDataService.listByType(typeId));
+    }
+}
